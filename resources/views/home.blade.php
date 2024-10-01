c53ac37f60b1f96ffee76c4b6f297206f50b3aac
Garry Nathanael <garry.nathanael@gmail.com>
2024-10-01 20:52:36 +0700

    Initial commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link
+        href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
+        rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH"
+        crossorigin="anonymous"
+    >
+</head>
+<body class="bg-dark text-light">
+    
+    <nav class="container-fluid p-3">
+        <div class="row">
+            <div class="col-9">Logo</div>
+            <div class="col-3">
+                <div class="row">
+                    <div class="col">Home</div>
+                    <div class="col">Product</div>
+                    <div class="col">About</div>
+                </div>
+            </div>
+        </div>
+    </nav>
+
+    <div class="p-3"></div>
+
+    <header class="container">
+        <div class="row">
+            <div class="col d-flex flex-column justify-content-center align-items-start">
+                <label class="fs-1 fw-bold">Sustainable Energy,</label>
+                <label class="fs-1 fw-bold text-warning">Accessible for Everyone</label>
+                <label class="fs-5 mt-3">Discover top-quality renewable energy products tailored to your needs. Join the movement towards a greener future.</label>
+                <button type="button" class="btn btn-primary mt-5">Explore Products</button>
+            </div>
+            <div class="col">
+                <img src="/panel_surya.jpg" alt="" class="img-fluid rounded-4" />
+            </div>
+        </div>
+    </header>
+
+    <script
+        src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+        crossorigin="anonymous"
+    ></script>
+
+</body>
+</html>
